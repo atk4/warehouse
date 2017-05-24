2b15f546247e290f3390d60dd5a7eef4f7b343c1
Romans Malinovskis <romans@agiletoolkit.org>
2017-05-24 22:30:51 +0100

    minor improvements

--- src/Article.php
+++ src/Article.php
@@ -16,7 +16,8 @@ class Article extends Model {
         $this->addField('name');
         $this->addField('description', ['type'=>'text']);
         $this->addField('measurement_unit', ['enum'=>['pc', 'kg', 'm', 'l' ]]);
-        $this->hasOne('primary_supplier_id', new Supplier());
+        $this->hasOne('primary_supplier_id', new Supplier())
+            ->addField('vendor', 'name');
         $this->addField('producer_product_code');
         $this->addField('brand');
 
@@ -32,7 +33,8 @@ class Article extends Model {
         $this->addField('width');
         $this->addField('length');
 
-        $this->addField('factor', ['enum'=>['pcs','box']]);
+        $this->addField('factor', ['caption'=>'Factor (pcs/box)']);
+//        $this->addField('is_active', ['type'=>'boolean']);
 
         $this->hasMany('Stock', new Stock())
             ->addField('stock', ['aggregate'=>'sum', 'field'=>'qty_increase']);
--- stock.php
+++ stock.php
@@ -5,10 +5,14 @@ $app = new Warehouse();
 
 $cr = $app->layout->add('CRUD');
 
-$cr->addColumn(new \atk4\ui\TableColumn\Template('<a href="article.php?article_id={$id}"><i class="stack overflow icon"></i> {$name}: {$stock} in stock</a>'));
+//$cr->addColumn(new \atk4\ui\TableColumn\Template('<a href="article.php?article_id={$id}"><i class="stack overflow icon"></i> {$name}: {$stock} in stock</a>'));
 
-$cr->fieldsGrid = ['category', 'brand', 'name'];
+$cr->fieldsGrid = ['id', 'name', 'vendor', 'category', 'purchase_price', 'sale_price'];
 $cr->setModel(new Article($app->db));
+
+$cr->addColumn('name', ['TableColumn/Link', 'article', 'args'=>['article_id'=>'id']]);
+
+$cr->addQuickSearch(['name','category','vendor']);
 
 
 $cr->menu->addItem(new ui\Manager(['Brands', 'icon'=>'tag']))
